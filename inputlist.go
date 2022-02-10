25e50c69c766ba98f91c58d277dbb57003ec3c73
levw-eats-tacos <eryan@insight.rr.com>
2022-02-10 18:16:54 -0500

    added method comment and multiple input string capability

--- inputlist.go
+++ inputlist.go
@@ -8,11 +8,19 @@ import (
 	"strings"
 )
 
-func GetListFloat64(format string) map[int]float64 {
+//TODO: Figure out templates/generics/interfaces
+/**
+ *  Description: gets a list of strings and then prompts for input based on them
+ *  @Param format:  list of prompt strings
+ *  @return: list of floats
+ */
+func GetListFloat64(format ...string) map[int]float64 {
 	cin := bufio.NewScanner(os.Stdin)
 	input := make(map[int]float64)
-	fmt.Print(format)
 	for i := 0; cin.Scan(); i++ {
+		if i < len(format) {
+			fmt.Print(format[i])
+		}
 		text := strings.TrimSpace(cin.Text())
 		if text == "" {
 			i--
