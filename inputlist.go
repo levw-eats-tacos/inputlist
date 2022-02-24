fb23c2b6b6b24afe47bbcceee482ab4e15cd6827
levw-eats-tacos <eryan@insight.rr.com>
2022-02-24 15:42:56 -0500

    Added GetListString

--- inputlist.go
+++ inputlist.go
@@ -56,3 +56,27 @@ func GetListInt64(format ...string) map[int]int64 {
 	}
 	return input
 }
+
+/**
+ *  Description: gets a list of strings and then prompts for input based on them
+ *  @Param format:  list of prompt strings
+ *  @return: list of ints
+ */
+func GetListString(format ...string) map[int]string {
+	cin := bufio.NewScanner(os.Stdin)
+
+	input := make(map[int]string)
+	fmt.Print(format[0])
+	for i := 1; cin.Scan(); i++ {
+		if i < len(format) {
+			fmt.Print(format[i])
+		}
+		text := strings.TrimSpace(cin.Text())
+		if text == "" {
+			i--
+			continue
+		}
+		input[i-1] = text
+	}
+	return input
+}
